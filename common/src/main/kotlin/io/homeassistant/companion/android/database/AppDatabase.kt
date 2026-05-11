643fbedbeea0b3d5a428c3990a6b29ac86dd210f
Mike Franklin <mfranklin@sampleserve.com>
2026-05-10 20:09:14 -0400

    Added migration

--- common/src/main/kotlin/io/homeassistant/companion/android/database/AppDatabase.kt
+++ common/src/main/kotlin/io/homeassistant/companion/android/database/AppDatabase.kt
@@ -77,7 +77,7 @@ import io.homeassistant.companion.android.database.widget.WidgetTapActionConvert
         Server::class,
         Setting::class,
     ],
-    version = 51,
+    version = 52,
     autoMigrations = [
         AutoMigration(from = 24, to = 25),
         AutoMigration(from = 25, to = 26),
@@ -104,6 +104,7 @@ import io.homeassistant.companion.android.database.widget.WidgetTapActionConvert
         AutoMigration(from = 48, to = 49),
         AutoMigration(from = 49, to = 50),
         AutoMigration(from = 50, to = 51),
+        AutoMigration(from = 51, to = 52),
     ],
 )
 @TypeConverters(
